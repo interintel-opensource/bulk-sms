4e29d5c3a0980ed5cb025411a92205619d157789
samson-arita <32759507+samson-arita@users.noreply.github.com>
2019-06-14 16:47:37 +0300

    Update Constants.java

--- Java/Constants.java
+++ Java/Constants.java
@@ -5,30 +5,13 @@ package ke.co.interintel.nikobizzapi;
  */
 public class Constants {
     public final static String SERVICE_GET_HOST = "get-my-host/";
-    public final static String SERVICE_HOME = "HOME";
-    public final static String SERVICE_INSTITUTION_PAGE = "INSTITUTION PAGE";
-    public final static String SERVICE_LOGIN = "LOGIN";
-    public final static String MIPAY_HOST = "mipaymobile.com";
+    public final static String SERVICE_SEND_SMS = "SEND SMS";
 
-    //Gateway Variables (Change to configure a diffferent gateway/Institution) //
-
-    //public final static String GATEWAY_HOST = "bidfather.com";
-    //public final static String GATEWAY_HOST = "mchaama.com";
-    //public final static String GATEWAY_HOST = "shikangoma.com";
-    public final static String GATEWAY_HOST = "nikobizz.com";
-    //public final static String GATEWAY_HOST = "mipaymobile.com";
-    //public final static String GATEWAY_HOST = "muziqbit.com";
-
-    public static final boolean IS_INSTITUTION = false;
-    public static final String INSTITUTION_ID = "35";
-    //End gateway variables
+    public final static String GATEWAY_HOST = "services.interintel.co";
 
     public final static String URL = "https://" + GATEWAY_HOST + "/api/";
 
-
-    public static final int CHID = 1;
-    public static final String SERVICE_REMIT = "REMIT";
-
+    public static final int CHID = 11;
 
     public static String MEDIA_TYPE_JSON = "application/json; charset=utf-8";
 }
