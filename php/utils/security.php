462174940e5d4f587817cc2baf645db3653148be
samson-arita <samson.arita@interintel.co.ke>
2019-07-10 03:31:22 -0400

    Updates

--- php/utils/security.php
+++ php/utils/security.php
@@ -1,6 +1,23 @@
 <?php
 define ('HMAC_SHA256', 'sha256');
 
+	function urlsafe_b64encode($string)
+	{
+	    $data = base64_encode($string);
+	    $data = str_replace(array('+', '/'), array('-', '_'), $data);
+	    return $data;
+	}
+
+	function urlsafe_b64decode($string)
+	{
+	    $data = str_replace(array('-', '_'), array('+', '/'), $string);
+	    $mod4 = strlen($data) % 4;
+	    if ($mod4) {
+		$data .= substr('====', $mod4);
+	    }
+	    return base64_decode($data);
+	}
+
 	function security($payload, $api_key){
 		$p = array();
 		$new_payload = array();
@@ -17,9 +34,14 @@ define ('HMAC_SHA256', 'sha256');
 			} 
 		$p1 = implode("&",$p);
 		print $p1;
+		/*
 		$secretKey = trim(base64_decode($api_key));
 		print $secretKey;
 		$a = base64_encode(hash_hmac(HMAC_SHA256, $p1, $secretKey, true));
+		*/
+		$secretKey = trim(urlsafe_b64decode($api_key));
+		print $secretKey;
+		$a = urlsafe_b64encode(hash_hmac(HMAC_SHA256, $p1, $secretKey, true));
 		$payload['sec_hash'] = $a;
 		return $payload;
 	}
